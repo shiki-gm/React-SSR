9c21993573287246863535c04ae53bb23f939b78
wangguangming <3211314336@qq.com>
2019-12-10 23:10:39 +0800

    初始化前后端工程

---
+++ webpack.client.js
@@ -0,0 +1,26 @@
+const path = require('path')
+// 规避node-module
+// const nodeExternals = require('webpack-node-externals')
+
+// 客户端的webpack
+module.exports = {
+  mode: 'development',
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // 支持import 支持jsx
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-react', ['@babel/preset-env']]
+        }
+      }
+    ]
+  }
+}
